822e328fde0e9fbfb7b16b35851aa31dbe8f798b
maxpawgdbs <maxim.taranenko17112008@gmail.com>
2025-12-18 12:21:50 +0300

    доделал запросы, принимает текст и картинку + пофиксил .gitignore

--- src/vlm/main.py
+++ src/vlm/main.py
@@ -11,17 +11,32 @@ class VLMAgent:
         self.model = f"gpt://{folder_id}/gemma-3-27b-it/latest"
         self.client = openai.OpenAI(
             api_key=token,
-            base_url="https://rest-assistant.api.cloud.yandex.net/v1",
+            base_url="https://llm.api.cloud.yandex.net/v1",
             project=folder_id,
         )
 
-    def request(self, filename: str, prompt: str) -> str:
+    def request(self, filename: str, prompt: str) -> str | None:
         with open(filename, "rb") as f:
             image_base64 = base64.b64encode(f.read()).decode("utf-8")
-        response = self.client.responses.create(
-            model=self.model, temperature=0.3, input=prompt, max_output_tokens=100
+        image_payload = (
+            f"data:image/{filename[filename.rfind('.') + 1 :]};base64,{image_base64}"
         )
-        return response.output_text
+        response = self.client.chat.completions.create(
+            model=self.model,
+            messages=[
+                {
+                    "role": "user",
+                    "content": [
+                        {"type": "text", "text": prompt},
+                        {"type": "image_url", "image_url": {"url": image_payload}},
+                    ],
+                }
+            ],
+            temperature=0.3,
+            max_tokens=200,
+        )
+
+        return response.choices[0].message.content
 
 
 if __name__ == "__main__":
@@ -35,4 +50,4 @@ if __name__ == "__main__":
         token=os.getenv("YANDEX_GPT_API_TOKEN", ""),
         folder_id=os.getenv("YANDEX_CLOUD_FOLDER_ID", ""),
     )
-    print(vision_agent.request("image.jpg", "привет кто на картинке"))
+    print(vision_agent.request("cat.jpg", "привет кто на картинке"))
